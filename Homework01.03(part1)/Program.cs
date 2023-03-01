6e8cdf73dda31249217f33696771d888da861ca6
mariam <mariam@MYPROBOOK>
2023-03-01 15:04:51 +0400

    Add project files.

---
+++ Homework01.03(part1)/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework01._03_part1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Restaurant restaurant = new Restaurant();
+
+            Console.WriteLine("How many Hot Dogs do you want?: ");
+            int a = int.Parse(Console.ReadLine());
+            Restaurant.MakeHotDog(a);
+
+            //restaurant.MakeHotDog(a);
+
+            Console.WriteLine("How many Sushis do you want?: ");
+            int b = int.Parse(Console.ReadLine());
+            Restaurant.MakeSushi(b);
+
+            //restaurant.MakeSushi(b);
+
+            Console.WriteLine("How many Burgers do you want?: ");
+            int c = int.Parse(Console.ReadLine());
+            Restaurant.MakeBurger(c);
+
+            //restaurant.MakeBurger(c);
+
+        }
+    }
+}
---
+++ Homework01.03(part1)/Restaurant.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework01._03_part1_
+{
+    internal class Restaurant
+    {
+        public static int MakeHotDog(ref int a)
+        {
+            int priceHotDog = 850;
+            int fPriceHotDog = a * priceHotDog;
+            Console.WriteLine(fPriceHotDog + " AMD");
+            return fPriceHotDog;
+        }
+        public static int MakeSushi(ref int b)
+        {
+            int priceSushi = 5500;
+            int fPriceSushi = b * priceSushi;
+            Console.WriteLine(fPriceSushi + " AMD");
+            return fPriceSushi;
+        }
+        public static int MakeBurger(ref int c)
+        {
+            int priceBurger = 2500;
+            int fPriceBurger = c * priceBurger;
+            Console.WriteLine(fPriceBurger + " AMD");
+            return fPriceBurger;
+        }
+        //public int Reserve()
+        //{
+        //    return 0;
+        //}
+        public Restaurant()
+        {
+
+        }//default ctor
+        public Restaurant(ref int a, int b, int c) //parametrized ctor
+        {
+            a = 1;
+            b = 1;
+            c = 1;
+            MakeHotDog(a);
+            MakeBurger(b);
+            MakeSushi(c);
+        }
+
+
+
+
+
+
+
+    }
+}
